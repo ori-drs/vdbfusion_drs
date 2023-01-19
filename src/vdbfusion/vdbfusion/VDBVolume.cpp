0dab30ae40faed657085683d19325beeb01b12f2
Joseph Rowell <joerowell98@hotmail.co.uk>
2023-01-19 13:47:48 +0000

    comparison functions

--- src/vdbfusion/vdbfusion/VDBVolume.cpp
+++ src/vdbfusion/vdbfusion/VDBVolume.cpp
@@ -187,29 +187,29 @@ void VDBVolume::CompareTSDFGrids(openvdb::FloatGrid::Ptr grid_1,
     }
 }
 
-const float VDBVolume::Accessor(const GridType sourceGrid,
-                         const GridType targetGrid,
-                         openvdb::Coord ijk) {
+// const float VDBVolume::Accessor(const GridType sourceGrid,
+//                          const GridType targetGrid,
+//                          openvdb::Coord ijk) {
 
-  // Instantiate the DualGridSampler template on the grid type and on
-  // a box sampler for thread-safe but uncached trilinear interpolation.
-  openvdb::tools::DualGridSampler<GridType, openvdb::tools::BoxSampler>
-      sampler(sourceGrid, targetGrid.constTransform());
-  // Compute the value of the source grid at a location in the
-  // target grid's index space.
-  GridType::ValueType value = sampler(ijk);
-  // Request a value accessor for accelerated access to the source grid.
-  // (Because value accessors employ a cache, it is important to declare
-  // one accessor per thread.)
-  GridType::ConstAccessor accessor = sourceGrid.getConstAccessor();
-  // Instantiate the DualGridSampler template on the accessor type and on
-  // a box sampler for accelerated trilinear interpolation.
-  openvdb::tools::DualGridSampler<GridType::ConstAccessor, openvdb::tools::BoxSampler>
-      fastSampler(accessor, sourceGrid.constTransform(), targetGrid.constTransform());
-  // Compute the value of the source grid at a location in the
-  // target grid's index space.
-  tsdf = fastSampler(ijk);
-  return tsdf;
-}
+//   // Instantiate the DualGridSampler template on the grid type and on
+//   // a box sampler for thread-safe but uncached trilinear interpolation.
+//   openvdb::tools::DualGridSampler<GridType, openvdb::tools::BoxSampler>
+//       sampler(sourceGrid, targetGrid.constTransform());
+//   // Compute the value of the source grid at a location in the
+//   // target grid's index space.
+//   GridType::ValueType value = sampler(ijk);
+//   // Request a value accessor for accelerated access to the source grid.
+//   // (Because value accessors employ a cache, it is important to declare
+//   // one accessor per thread.)
+//   GridType::ConstAccessor accessor = sourceGrid.getConstAccessor();
+//   // Instantiate the DualGridSampler template on the accessor type and on
+//   // a box sampler for accelerated trilinear interpolation.
+//   openvdb::tools::DualGridSampler<GridType::ConstAccessor, openvdb::tools::BoxSampler>
+//       fastSampler(accessor, sourceGrid.constTransform(), targetGrid.constTransform());
+//   // Compute the value of the source grid at a location in the
+//   // target grid's index space.
+//   tsdf = fastSampler(ijk);
+//   return tsdf;
+// }
 
 }  // namespace vdbfusion
